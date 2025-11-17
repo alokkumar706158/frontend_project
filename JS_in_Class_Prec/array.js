4a2731acbc67df4881a2055915caa01dbc7907ab
ALOK KUMAR <225845655+alokkumar706158@users.noreply.github.com>
2025-11-17 12:57:50 +0530

    Delete JS_in_Class_Prec directory

--- JS_in_Class_Prec/array.js
+++
@@ -1,5 +0,0 @@
-let n=[1,2,3,4,5];
-let a=[]
-for(let i=0; i<n; i++){
-    console.log(a[i]);
-}
--- JS_in_Class_Prec/ternary.js
+++
@@ -1,6 +0,0 @@
-let age = 18;
-
-let result = (age > 19) ? "Not eligible" : 
-             (age > 12) ? "Eligible" : 
-             
-console.log(result);
