9fd31b017433119671452ac0f38b31420bf89c18
Alok kumare <alok706158@gmail.com>
2025-11-20 23:06:46 +0530

    learing react

---
+++ Task/script.js
@@ -0,0 +1,2 @@
+const board=document.querySelector(".chessboard");
+for(let row=1; row)
--- javaScript_Youtube/dataType.js
+++
@@ -1,4 +0,0 @@
-"Use Strict" //treat all js code as newer version
-
-// alert("hii") // alert word on browser
-
